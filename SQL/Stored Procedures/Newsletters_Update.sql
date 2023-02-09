0bd1e5ad0a16f2b3105ec87187a354148ff551e6
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 18:59:29 -0500

    Update Newsletters_Update.sql

--- SQL/Stored Procedures/Newsletters_Update.sql
+++ SQL/Stored Procedures/Newsletters_Update.sql
@@ -11,44 +11,44 @@
 -- ==============================================
 
 ALTER   PROC [dbo].[Newsletters_Update]
-             @TemplateId int
-            ,@Name nvarchar(100)
-            ,@CoverPhoto nvarchar(255)
-            ,@DateToPublish datetime2(7)
-            ,@DateToExpire datetime2(7)
-            ,@CreatedBy int
-            ,@Id int OUTPUT
-
+		@TemplateId int
+		,@Name nvarchar(100)
+		,@CoverPhoto nvarchar(255)
+		,@DateToPublish datetime2(7)
+		,@DateToExpire datetime2(7)
+		,@CreatedBy int
+		,@Id int OUTPUT
+	
 
 AS 
 
 /* ------ TEST CODE ------
 			
-      DECLARE  @Id int = 19
-            ,@TemplateId int = 18
-            ,@Name nvarchar(100) = 'Mugiwara Luffy Becomes Pirate King III'
-            ,@CoverPhoto nvarchar(100) = 'https://bit.ly/3Qww6Cb'
-            ,@DateToPublish datetime2(7) = '2022-08-19'
-            ,@DateToExpire datetime2(7) = '2023-10-08'
-            ,@CreatedBy int = 22
+	DECLARE  @Id int = 19
+		,@TemplateId int = 18
+		,@Name nvarchar(100) = 'Spring Technology Newsletter'
+		,@CoverPhoto nvarchar(100) = 'https://bit.ly/3Qww6Cb'
+		,@DateToPublish datetime2(7) = '2022-04-15'
+		,@DateToExpire datetime2(7) = '2022-06-15'
+		,@CreatedBy int = 22
 
-			EXECUTE dbo.Newsletters_Update					 
-             @TemplateId
-            ,@Name
-            ,@CoverPhoto
-            ,@DateToPublish
-            ,@DateToExpire
-            ,@CreatedBy
-            ,@Id OUTPUT 
+	EXECUTE dbo.Newsletters_Update					 
+		@TemplateId
+		,@Name
+		,@CoverPhoto
+		,@DateToPublish
+		,@DateToExpire
+		,@CreatedBy
+		,@Id OUTPUT 
 
-			SELECT * 
-			FROM dbo.Newsletters
-			
-			SELECT * 
-			FROM dbo.NewsletterTemplates
+	SELECT * 
+	FROM dbo.Newsletters
 
-			SELECT * 
-			FROM dbo.Users
+	SELECT * 
+	FROM dbo.NewsletterTemplates
+
+	SELECT * 
+	FROM dbo.Users
 
 ------ END TEST CODE ------
 */
@@ -57,17 +57,17 @@ AS
 BEGIN
 
 
-			DECLARE @DateNow datetime2(7) = GETUTCDATE()
+	DECLARE @DateNow datetime2(7) = GETUTCDATE()
 
-			UPDATE [dbo].[Newsletters]
-            SET [TemplateId] = @TemplateId
-            ,[Name] = @Name
-            ,[CoverPhoto] = @CoverPhoto
-            ,[DateToPublish] = @DateToPublish
-            ,[DateToExpire] = @DateToExpire
-            ,[CreatedBy] = @CreatedBy
-            ,[DateModified] = @DateNow
-			 WHERE Id = @Id 
+	UPDATE [dbo].[Newsletters]
+		SET [TemplateId] = @TemplateId
+		,[Name] = @Name
+		,[CoverPhoto] = @CoverPhoto
+		,[DateToPublish] = @DateToPublish
+		,[DateToExpire] = @DateToExpire
+		,[CreatedBy] = @CreatedBy
+		,[DateModified] = @DateNow
+	 WHERE Id = @Id 
 
 
  END 
