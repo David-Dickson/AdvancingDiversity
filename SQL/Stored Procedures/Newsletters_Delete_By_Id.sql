a3bce1182864f501ed26e14a4e2702bd687f1813
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 16:58:48 -0500

    Create Newsletters_Delete_By_Id.sql

---
+++ SQL/Stored Procedures/Newsletters_Delete_By_Id.sql
@@ -0,0 +1,39 @@
+-- =============================================
+    -- Author: David Dickson
+    -- Create date: MM/DD/YEAR
+    -- Description: Delete for dbo.Newsletter_Delete_By_Id given the Id of Newsletters
+    -- Code Reviewer: Redacted 
+
+    -- MODIFIED BY: Author
+    -- MODIFIED DATE: MM/DD/YEAR
+    -- Code Reviewer:
+    -- Note:
+-- ==============================================
+
+ALTER   PROC [dbo].[Newsletters_Delete_By_Id]
+					@Id int
+
+
+AS
+
+/*---------TEST CODE---------
+	DECLARE @Id int = 15
+
+	EXECUTE [Newsletters_Delete_By_Id] @Id 
+
+	SELECT * FROM dbo.Newsletters
+
+---------END TEST CODE---------
+*/
+
+
+
+
+BEGIN
+
+
+		DELETE FROM [dbo].[Newsletters]
+		    WHERE Id = @Id
+
+
+END
