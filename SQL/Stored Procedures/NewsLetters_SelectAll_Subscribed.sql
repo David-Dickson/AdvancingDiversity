2ef070c1e93818d334c0443bb29a3dee81e97e2c
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 17:15:11 -0500

    Update NewsLetters_SelectAll_Subscribed.sql

--- SQL/Stored Procedures/NewsLetters_SelectAll_Subscribed.sql
+++ SQL/Stored Procedures/NewsLetters_SelectAll_Subscribed.sql
@@ -1,34 +1,35 @@
 ALTER proc [dbo].[NewsLetters_SelectAll_Subscribed]
-    @PageIndex int
-    ,@PageSize int
+    		@PageIndex int
+    		,@PageSize int
 						
 as
 
 /*-----------------TEST CODE-------------------
 
-      DECLARE @PageIndex int = 0
-					    ,@PageSize int = 3
+	DECLARE @PageIndex int = 0
+		,@PageSize int = 3
 
-			EXECUTE dbo.Newsletters_SelectAll 
-              @PageIndex
-							,@PageSize
+	EXECUTE dbo.Newsletters_SelectAll 
+		@PageIndex
+		,@PageSize
 
 ------------------------------------*/
 
 BEGIN
 
-			DECLARE @offset int = @PageIndex * @PageSize
+	DECLARE @offset int = @PageIndex * @PageSize
 
-			SELECT Email
-					  ,IsSubscribed as BitType
-					  ,DateCreated
-					  ,DateModified
-            
-			FROM dbo.NewsletterSubscriptions
-			WHERE IsSubscribed = 1
+	SELECT Email
+		,IsSubscribed as BitType
+	  	,DateCreated
+	  	,DateModified
 
-			ORDER BY Id
-			OFFSET @offset ROWS
-			FETCH NEXT @PageSize ROWS Only 
+	FROM dbo.NewsletterSubscriptions
+	WHERE IsSubscribed = 1
+
+	ORDER BY Id
+	
+	OFFSET @offset ROWS
+	FETCH NEXT @PageSize ROWS Only 
 
 END
