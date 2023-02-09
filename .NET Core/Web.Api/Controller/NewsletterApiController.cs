5e8e5d24c893170fd5cc200fe82c42360943b37c
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 13:41:59 -0500

    Create NewsletterApiController.cs

---
+++ .NET Core/Web.Api/Controller/NewsletterApiController.cs
@@ -0,0 +1,130 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Hidden.Models;
+using Hidden.Models.Domain.Newsletters;
+using Hidden.Models.Requests.Newsletters;
+using Hidden.Services;
+using Hidden.Services.Interfaces;
+using Hidden.Web.Controllers;
+using Hidden.Web.Models.Responses;
+using System;
+
+namespace Hidden.Web.Api.Controllers
+{
+    [Route("api/newsletters")]
+    [ApiController]
+    public class NewsletterApiController : BaseApiController
+    {
+        private INewsletterService _service = null;
+        private IAuthenticationService<int> _authService = null;
+
+        public NewsletterApiController(INewsletterService service 
+            , ILogger<NewsletterApiController> logger
+            , IAuthenticationService<int> authService) : base(logger)
+        {
+            _service = service;
+            _authService = authService;
+        }
+
+        [HttpGet("paginate")]
+        public ActionResult<ItemResponse<Paged<Newsletters>>> getPage(int pageIndex, int pageSize)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                Paged<Newsletters> page = _service.GetNewslettersPaginated(pageIndex, pageSize);
+
+                if (page == null)
+                {
+                    code = 404;
+                    response = new ErrorResponse("App Resource Not Found.");
+                }
+                else
+                {
+                    response = new ItemResponse<Paged<Newsletters>> { Item = page };
+                }
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+            return StatusCode(code, response);
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult<SuccessResponse> Delete(int id)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                _service.DeleteNewsletter(id);
+
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+        }
+
+        [HttpPut("{id:int}")]
+        public ActionResult<ItemResponse<int>> Update(NewsletterUpdateRequest model)
+        {
+            int code = 200;
+            BaseResponse response = null;
+
+            try
+            {
+                int userId = _authService.GetCurrentUser().Id;
+                _service.UpdateNewsletter(model, userId);
+                response = new SuccessResponse();
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex.ToString());
+                code = 500;
+                response = new ErrorResponse(ex.Message);
+            }
+
+            return StatusCode(code, response);
+
+
+        }
+
+        public ActionResult<ItemResponse<int>> Create(NewsletterAddRequest model)
+        {
+            ObjectResult result = null;
+
+            try
+            {
+                int userId = _authService.GetCurrentUser().Id;
+                int id = _service.AddNewsletter(model, userId);
+                ItemResponse<int> response = new ItemResponse<int>() { Item = id };
+                result = Created201(response);
+
+            }
+            catch (Exception ex) 
+            {
+                Logger.LogError(ex.ToString());
+                ErrorResponse response = new ErrorResponse(ex.Message);
+                result = StatusCode(500, response);
+
+            }
+
+            return result;
+
+        }
+
+    }
+
+}
