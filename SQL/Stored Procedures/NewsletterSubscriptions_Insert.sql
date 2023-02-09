001de49c4fbf091e9d286f5b24471c2be8c0014e
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 17:31:22 -0500

    Create NewsletterSubscriptions_Insert.sql

---
+++ SQL/Stored Procedures/NewsletterSubscriptions_Insert.sql
@@ -0,0 +1,51 @@
+-- =============================================
+    -- Author: David Dickson
+    -- Create date: MM/DD/YEAR
+    -- Description: Insert for dbo.NewsletterSubscriptions
+    -- Code Reviewer: Redacted 
+
+    -- MODIFIED BY: Author
+    -- MODIFIED DATE: MM/DD/YEAR
+    -- Code Reviewer:
+    -- Note:
+-- ==============================================
+
+ALTER PROC [dbo].[NewsletterSubscriptions_Insert]
+            @Email nvarchar(255)
+            ,@IsSubscribed bit
+            ,@Id int OUTPUT
+				
+AS
+
+/*---------------TEST CODE--------------
+		
+
+		SELECT * 
+		From dbo.NewsletterSubscriptions
+
+		DECLARE @Id int = 0
+		DECLARE @Email nvarchar(255) = 'Test32@gmail.com'
+				     ,@IsSubscribed bit = 1
+		
+		EXECUTE dbo.NewsletterSubscriptions_Insert 
+            @Email
+            ,@IsSubscribed
+            ,@Id OUTPUT
+													
+		SELECT * 
+		FROM dbo.NewsletterSubscriptions
+	
+---------------------------------------*/
+
+BEGIN
+
+			INSERT INTO dbo.NewsletterSubscriptions
+            ([Email]
+            ,[IsSubscribed])
+      VALUES
+            (@Email
+            ,@IsSubscribed)
+		
+			SET @Id = SCOPE_IDENTITY()
+			
+END
