a6f63b4844d67ebee08e94d7e18251bcf4fa740b
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 17:06:00 -0500

    Update Newsletters_Insert.sql

--- SQL/Stored Procedures/Newsletters_Insert.sql
+++ SQL/Stored Procedures/Newsletters_Insert.sql
@@ -2,7 +2,7 @@
     -- Author: David Dickson
     -- Create date: M/DD/YEAR
     -- Description: Insert records for dbo.Newsletters_Insert with the parameters listed below
-    -- Code Reviewer: Jorge Calderon
+    -- Code Reviewer: Redacted
 
     -- MODIFIED BY: Author
     -- MODIFIED DATE: M/DD/YEAR
@@ -11,44 +11,44 @@
 -- ==============================================
 
 ALTER   PROC [dbo].[Newsletters_Insert]
-					 @TemplateId int
-					,@Name nvarchar(100)
-					,@CoverPhoto nvarchar(255)
-					,@DateToPublish datetime2(7)
-					,@DateToExpire datetime2(7)
-					,@CreatedBy int
-					,@Id int OUTPUT
+		 @TemplateId int
+		,@Name nvarchar(100)
+		,@CoverPhoto nvarchar(255)
+		,@DateToPublish datetime2(7)
+		,@DateToExpire datetime2(7)
+		,@CreatedBy int
+		,@Id int OUTPUT
 
 AS
 
 /*
 ------ TEST CODE ------
 
-			DECLARE @Id int = 0
-					,@TemplateId int = 52
-					,@Name nvarchar(100) = 'Test Once '
-					,@CoverPhoto nvarchar(100) = 'https://bit.ly/3Qww6Cb'
-					,@DateToPublish datetime2(7) = '2022-09-18'
-					,@DateToExpire datetime2(7) = '2022-10-23'
-					,@CreatedBy int = 41
+	DECLARE @Id int = 0
+		,@TemplateId int = 52
+		,@Name nvarchar(100) = 'Test Once '
+		,@CoverPhoto nvarchar(100) = 'https://bit.ly/3Qww6Cb'
+		,@DateToPublish datetime2(7) = '2022-09-18'
+		,@DateToExpire datetime2(7) = '2022-10-23'
+		,@CreatedBy int = 41
 
-			EXECUTE dbo.Newsletters_Insert 					 
-					 @TemplateId
-					,@Name
-					,@CoverPhoto
-					,@DateToPublish
-					,@DateToExpire
-					,@CreatedBy
-					,@Id OUTPUT 
+	EXECUTE dbo.Newsletters_Insert 					 
+		 @TemplateId
+		,@Name
+		,@CoverPhoto
+		,@DateToPublish
+		,@DateToExpire
+		,@CreatedBy
+		,@Id OUTPUT 
 
-			SELECT * 
-			FROM dbo.Newsletters
-			
-			SELECT * 
-			FROM dbo.NewsletterTemplates
+	SELECT * 
+	FROM dbo.Newsletters
 
-			SELECT * 
-			FROM dbo.Users
+	SELECT * 
+	FROM dbo.NewsletterTemplates
+
+	SELECT * 
+	FROM dbo.Users
 
 			
 ------ END TEST CODE ------
@@ -57,23 +57,23 @@ AS
 BEGIN 
 		  
 			
-			INSERT INTO [dbo].[Newsletters]
-					   ([TemplateId]
-					   ,[Name]
-					   ,[CoverPhoto]
-					   ,[DateToPublish]
-					   ,[DateToExpire]
-					   ,[CreatedBy])
-				 VALUES
-					   (@TemplateId,
-              @Name,
-              @CoverPhoto,
-              @DateToPublish,
-              @DateToExpire,
-              @CreatedBy
-              )
+	INSERT INTO [dbo].[Newsletters]
+		([TemplateId]
+		,[Name]
+		,[CoverPhoto]
+		,[DateToPublish]
+		,[DateToExpire]
+		,[CreatedBy])
+ 	VALUES
+	   	(@TemplateId,
+		@Name,
+		@CoverPhoto,
+		@DateToPublish,
+		@DateToExpire,
+		@CreatedBy
+		)
 				
 			
-				SET @Id = SCOPE_IDENTITY()
+	SET @Id = SCOPE_IDENTITY()
 
 END
