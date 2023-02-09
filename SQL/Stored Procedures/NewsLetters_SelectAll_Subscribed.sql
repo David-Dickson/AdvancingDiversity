fc547f581f9e0964aec7c9334b7ee30d7222d63f
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 17:13:07 -0500

    Create NewsLetters_SelectAll_Subscribed.sql

---
+++ SQL/Stored Procedures/NewsLetters_SelectAll_Subscribed.sql
@@ -0,0 +1,34 @@
+ALTER proc [dbo].[NewsLetters_SelectAll_Subscribed]
+    @PageIndex int
+    ,@PageSize int
+						
+as
+
+/*-----------------TEST CODE-------------------
+
+      DECLARE @PageIndex int = 0
+					    ,@PageSize int = 3
+
+			EXECUTE dbo.Newsletters_SelectAll 
+              @PageIndex
+							,@PageSize
+
+------------------------------------*/
+
+BEGIN
+
+			DECLARE @offset int = @PageIndex * @PageSize
+
+			SELECT Email
+					  ,IsSubscribed as BitType
+					  ,DateCreated
+					  ,DateModified
+            
+			FROM dbo.NewsletterSubscriptions
+			WHERE IsSubscribed = 1
+
+			ORDER BY Id
+			OFFSET @offset ROWS
+			FETCH NEXT @PageSize ROWS Only 
+
+END
