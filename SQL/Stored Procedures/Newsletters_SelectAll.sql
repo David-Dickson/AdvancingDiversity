7f03cfcd55fb3d792ff434759e26967765fdd717
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 17:02:54 -0500

    Create Newsletters_SelectAll.sql

---
+++ SQL/Stored Procedures/Newsletters_SelectAll.sql
@@ -0,0 +1,65 @@
+-- =============================================
+    -- Author: David Dickson
+    -- Create date: M/DD/YEAR
+    -- Description: SelectAll Pagination for the given PageIndex and PageSized of Newsletters
+    -- Code Reviewer: Redacted
+
+    -- MODIFIED BY: Author
+    -- MODIFIED DATE: M/DD/YEAR
+    -- Code Reviewer:
+    -- Note:
+-- ==============================================
+
+ALTER   PROC [dbo].[Newsletters_SelectAll]
+			@PageIndex int
+		    ,@PageSize int
+
+
+AS
+
+
+/*------ TEST CODE ------
+	
+		DECLARE @PageIndex int = 0
+				,@PageSize int = 10
+
+		EXECUTE dbo.Newsletters_SelectAll
+				@PageIndex
+				,@PageSize
+
+        SELECT * 
+			FROM dbo.Newsletters
+			
+		SELECT * 
+			FROM dbo.NewsletterTemplates
+
+		SELECT * 
+			FROM dbo.Users
+
+
+------ END  TEST CODE -------
+*/
+
+
+BEGIN
+		DECLARE @offset int = @PageIndex * @PageSize
+
+		
+
+		SELECT [Id]
+			  ,TemplateId
+			  ,[Name]
+			  ,[CoverPhoto]
+			  ,[DateToPublish]
+			  ,[DateToExpire]
+			  ,[DateCreated]
+			  ,[DateModified]
+			  ,CreatedBy
+			  ,TotalCount = COUNT(1) OVER()
+          FROM dbo.Newsletters
+
+		  ORDER BY Id DESC
+		  OFFSET @offset ROWS
+		  FETCH NEXT @PageSize ROWS Only 
+
+END
