5af7279c004005f8996fef6ab5113f83461d4596
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 20:16:06 -0500

    Update NewsletterSubscriptions_Insert.sql

--- SQL/Stored Procedures/NewsletterSubscriptions_Insert.sql
+++ SQL/Stored Procedures/NewsletterSubscriptions_Insert.sql
@@ -21,30 +21,30 @@ AS
 		
 
 	SELECT * 
-	FROM dbo.NewsletterSubscriptions
+		FROM [dbo].[NewsletterSubscriptions]
 
 	DECLARE @Id int = 0
 	DECLARE @Email nvarchar(255) = 'Test32@gmail.com'
 		,@IsSubscribed bit = 1
 		
-	EXECUTE dbo.NewsletterSubscriptions_Insert 
+	EXECUTE [dbo].[NewsletterSubscriptions_Insert] 
 		@Email
 		,@IsSubscribed
 		,@Id OUTPUT
 													
 	SELECT * 
-	FROM dbo.NewsletterSubscriptions
+		FROM [dbo].[NewsletterSubscriptions]
 	
 ---------------------------------------*/
 
 BEGIN
 
-	INSERT INTO dbo.NewsletterSubscriptions
-            ([Email]
-            ,[IsSubscribed])
+	INSERT INTO [dbo].[NewsletterSubscriptions]
+		[Email]
+		,[IsSubscribed]
 	VALUES
-            (@Email
-            ,@IsSubscribed)
+		@Email
+		,@IsSubscribed
 		
 	SET @Id = SCOPE_IDENTITY()
 			
