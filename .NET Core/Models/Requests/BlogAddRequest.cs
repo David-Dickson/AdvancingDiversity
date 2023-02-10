d47367f6afa099e7f5f65536c244fabd943b61e2
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 22:18:49 -0500

    Create BlogAddRequest.cs

---
+++ .NET Core/Models/Requests/BlogAddRequest.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hidden.Models.Requests.Blog
+{
+    public class BlogAddRequest
+    {
+        [Required]
+        public int BlogTypeId { get; set; }
+
+        [Required]
+        public int AuthorId { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(120)]
+        public string Title { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(120)]
+        public string Subject { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(255)]
+        public string Content { get; set; }
+
+        [Required]
+        public bool IsPublished { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(255)]
+        public string ImageUrl { get; set; }
+
+    }
+}
