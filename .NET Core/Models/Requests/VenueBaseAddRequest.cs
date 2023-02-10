eec2cbdc8aa8d7a3fcdecc01f237034faf0d9544
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-10 16:51:30 -0500

    Create VenueBaseAddRequest.cs

---
+++ .NET Core/Models/Requests/VenueBaseAddRequest.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hidden.Models.Requests.Events
+{
+    public class VenueBaseAddRequest
+    {
+        [Required]
+        [StringLength(255, MinimumLength = 2)]
+        public string VenueName { get; set; }
+
+        [Required]
+        [StringLength(4000, MinimumLength = 2)]
+        public string VenueDescription { get; set; }
+
+        [Required]
+        [StringLength(255, MinimumLength = 2)]
+        public string VenueUrl { get; set; }
+
+        [Required]
+        public Locations.LocationAddRequest Location { get; set; }
+    }
+}
