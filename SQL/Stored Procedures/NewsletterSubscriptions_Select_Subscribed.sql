5686803c3966152bf66387cec6b171eee5af8fcf
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 19:08:14 -0500

    Create NewsletterSubscriptions_Select_Subscribed.sql

---
+++ SQL/Stored Procedures/NewsletterSubscriptions_Select_Subscribed.sql
@@ -0,0 +1,40 @@
+--===========================================================================================
+-- Author: David Dickson
+-- Create date: MM/DD/YEAR
+-- Description: [dbo].[NewsletterSubscriptions_SelectAll_Subscribed] for dbo.NewsletterSubscriptions
+-- Code Reviewer: Redacted
+
+-- Modified by: author
+-- Modified date: MM/DD/YEAR
+-- Code Reviewer: Oluwatosin Ehindro
+-- Note:
+--============================================================================================
+
+
+
+ALTER PROC [dbo].[NewsletterSubscriptions_Select_Subscribed]
+                  @IsSubscribed bit
+
+AS
+
+/*-----------TEST CODE---------------------------------------
+			
+			DECLARE @IsSubscribed bit = 'True'	
+
+			EXECUTE dbo.NewsletterSubscriptions_Select_Subscribed 
+									@IsSubscribed
+
+-----------------------------------------------------------*/
+
+BEGIN
+
+			SELECT Id
+                  ,Email
+                  ,IsSubscribed
+                  ,DateCreated
+                  ,DateModified
+					
+			FROM dbo.NewsletterSubscriptions
+			WHERE IsSubscribed = @IsSubscribed
+
+END
