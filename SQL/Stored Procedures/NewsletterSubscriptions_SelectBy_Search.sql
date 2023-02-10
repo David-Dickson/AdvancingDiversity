708f58281ec7936e9a38d138b37a382e30dd781c
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 19:18:37 -0500

    Create NewsletterSubscriptions_SelectBy_Search.sql

---
+++ SQL/Stored Procedures/NewsletterSubscriptions_SelectBy_Search.sql
@@ -0,0 +1,40 @@
+ALTER PROC [dbo].[NewsletterSubscriptions_SelectBy_Search]
+							@PageIndex int
+							,@PageSize int
+							,@Query nvarchar(255)
+
+AS
+
+/*-----------TEST CODE----------------
+			
+			SELECT *
+			FROM [dbo].[NewsletterSubScriptions]
+
+			DECLARE @PageIndex int = 0
+					,@PageSize int = 10
+					,@Query nvarchar(255) = 'Test06@gmail.com'
+
+			EXECUTE [dbo].[NewsletterSubscriptions_SelectBy_Search]
+									@PageIndex
+									,@PageSize
+									,@Query
+
+------------------------------------------------------------------*/
+
+BEGIN
+
+			DECLARE @offset int = @PageIndex * @PageSize
+			
+			SELECT [Id]
+					,[Email]
+					,[IsSubscribed]
+					,[DateCreated]
+					,[DateModified]
+					,TotalCount = COUNT(1) OVER()
+			FROM dbo.NewsletterSubscriptions
+			WHERE (Email LIKE '%' + @Query + '%')
+			ORDER BY Id
+			OFFSET @offset ROWS
+			FETCH NEXT @PageSize ROWS ONLY
+
+END
