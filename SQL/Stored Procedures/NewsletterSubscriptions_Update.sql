77717b7e8d3dd238671f70c1e1f156154a8f3a21
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-09 19:28:22 -0500

    Create NewsletterSubscriptions_Update.sql

---
+++ SQL/Stored Procedures/NewsletterSubscriptions_Update.sql
@@ -0,0 +1,45 @@
+--===========================================================================================
+-- Author: David Dickson
+-- Create date: MM/DD/YEAR
+-- Description: [dbo].[NewsletterSubscriptions_Update] for dbo.NewsletterSubscriptions
+-- Code Reviewer: Redacted
+
+-- Modified by: author
+-- Modified date: MM/DD/YEAR
+-- Code Reviewer: 
+-- Note:
+--============================================================================================
+
+
+
+ALTER PROC [dbo].[NewsletterSubscriptions_Update]
+								@Email nvarchar(255)
+								,@IsSubscribed bit
+
+AS
+
+/*-----------TEST CODE-----------------
+		
+			DECLARE @Email nvarchar(255) = 'Test01@email.com'
+					,@IsSubscribed bit = 0
+					
+			EXECUTE [dbo].[NewsletterSubscriptions_Update]
+						@Email
+						,@IsSubscribed
+
+			SELECT *
+			FROM [dbo].[NewsletterSubscriptions]
+			WHERE @Email = Email
+	
+-------------------------------------*/
+
+BEGIN 
+
+			DECLARE @DateNow datetime2 = getutcdate()
+
+			UPDATE [dbo].[NewsletterSubscriptions]
+			SET		IsSubscribed = @IsSubscribed
+					,DateModified = @DateNow
+			WHERE @Email = Email
+			
+END
