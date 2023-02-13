5689f099debdeeb9c18acb67e75a6e033b00f772
David Dickson <49521754+David-Dickson@users.noreply.github.com>
2023-02-12 20:03:20 -0500

    Create JobType.cs

---
+++ .NET Core/Models/Domain/JobType.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sabio.Models.Domain.JobTypes
+{
+    public class JobType
+    {
+        public int Id { get; set; }
+        
+        public string Type { get; set; }
+    }
+}
